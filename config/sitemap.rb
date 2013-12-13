1f5fdd7fceda8336ce0fe6ff1248cf8f611bb127
alex <alesssandro@ukr.net>
2013-12-13 13:44:43 +0200

    modify config/sitemap.rb

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,11 +1,14 @@
 SitemapGenerator::Sitemap.default_host = "http://is-valid.org"
 # if change this path crashed sitemap tab content in admin panel
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+SitemapGenerator::Sitemap.include_root = false
 
 SitemapGenerator::Sitemap.create do
-
-  Post.where(:status => false).each do |post|
-    add show_post_comments_path(post), :lastmod => post.updated_at
+  add root_path, :lastmod => false, :priority => 1.0, :changefreq => false
+  Post.where(:status => false).order(:updated_at).reverse_order.find_in_batches do |group|
+    group.each do |post|
+      add show_post_comments_path(post), :lastmod => false, :priority => 0.8, :changefreq => false
+    end
   end
 end
 
