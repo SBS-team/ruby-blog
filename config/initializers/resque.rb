a0000873ec64e7909121da882cd8114ab18ab550
alex <alesssandro@ukr.net>
2013-12-15 16:41:16 +0200

    add *~ to gitignore, now dont commit backup files

--- config/initializers/resque.rb
+++
@@ -1,8 +0,0 @@
-Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
-#rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
-#rails_env = ENV['RAILS_ENV'] || 'development'
-
-#resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-#Resque.redis = resque_config[rails_env]
-Resque.redis = 'localhost:6379'
-Resque.enqueue(SitemapUpdateJob)
