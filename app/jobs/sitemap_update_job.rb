11177b469ae603a81acaf853cfceeb4892b89637
alex <alesssandro@ukr.net>
2013-12-15 16:45:54 +0200

    configure resque and schedule

--- app/jobs/sitemap_update_job.rb
+++ app/jobs/sitemap_update_job.rb
@@ -2,17 +2,13 @@ class SitemapUpdateJob
   @queue = :sitemap_updating
 
   def self.perform
-    puts "Begin sitemap.xml update"
+    puts "\nFILE sitemap.xml.gz UPDATING..."
     require 'rake'
     app = Rake.application
     app.init
-    # do this as many times as needed
-    #app.add_import 'some/other/file.rake'
-    # this loads the Rakefile and other imports
     app.load_rakefile
     app['sitemap:refresh:no_ping'].invoke
     app['sitemap:refresh:no_ping'].reenable
-    puts "---FINISH---"
-    puts "File sitemap.xml.gz was successfully updated."
+    puts "File sitemap.xml.gz was SUCCESSFULLY updated.\n---FINISH JOB---"
   end
 end
