4b6bdb8039d30b2ac21542707acf5ae3f2c10b15
Vitaly <realnatisk@gmail.com>
2013-12-17 13:21:10 +0200

    Update posts to create slug

--- db/migrate/20131210100736_add_index_to_post.rb
+++ db/migrate/20131210100736_add_index_to_post.rb
@@ -2,5 +2,6 @@ class AddIndexToPost < ActiveRecord::Migration
   def change
     add_column :posts, :slug, :string
     add_index :posts, :slug, unique: true
+    Post.all.map{|x| x.save!} #Update posts to create slug
   end
 end
