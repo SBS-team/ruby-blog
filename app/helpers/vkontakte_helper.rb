541da83b8f984c4eeb8448f8422c1a3ced585d94
alex <alesssandro@ukr.net>
2013-12-19 13:55:10 +0200

    add vkontakte_helper with method repost_on_group_wall

---
+++ app/helpers/vkontakte_helper.rb
@@ -0,0 +1,18 @@
+module VkontakteHelper
+  include ActionView::Helpers::TextHelper
+
+  def repost_on_group_wall(post)
+    require 'yaml'
+    vk_params = YAML.load_file(File.join(Rails.root, 'config', 'application.yml'))[Rails.env]['vk']
+    vk = VkontakteApi::Client.new(vk_params['access_token'])
+    #чтобы вместо http://localhost... добавлялась на стену реальная ссылка
+    url = Rails.env =~ /production/ ? show_post_comments_url(post) : 'http://pogoda.yandex.ua'
+    message = "#{post.subject}\r\n#{truncate(post.message.strip_tags, length: post.truncate_character)}\r\n\r\nУзнать подробнее на #{url}"
+    response = vk.wall.post( owner_id: "-#{vk_params['group_id']}",
+                             message: message,
+                             from_group: vk_params['from_group'],
+                             signed: vk_params['signed'])
+    response['error'].blank? && response['post_id']
+  end
+
+end
