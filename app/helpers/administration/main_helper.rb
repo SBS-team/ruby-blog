2eeb0fd46e21b7ed91ae30deb099d32b4b04b984
Vitaly <realnatisk@gmail.com>
2013-12-27 18:42:56 +0200

    change helper for administration active link

--- app/helpers/administration/main_helper.rb
+++ app/helpers/administration/main_helper.rb
@@ -1,5 +1,9 @@
 module Administration::MainHelper
-  def has_active_class?(href)
-    current_page?(href) ? {:class => "active"} : []
+
+  def nav_link_to(link_text, link_path, controller_name)
+    content_tag :li, class: (controller.controller_name == controller_name.to_s ? 'active' : '') do
+      link_to link_text, link_path
+    end
   end
+
 end
